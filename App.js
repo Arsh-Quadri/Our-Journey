a37d5a47e0e479ccbccdd3a9630161c1b354d764
Arsh-Quadri <118104906+Arsh-Quadri@users.noreply.github.com>
2022-11-13 15:41:56 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,74 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="box container">
+      <div className="container">
+        <h1 className="head1">December 2020</h1><br />
+        <h1 className="head2">Journey Begins</h1>
+        <div className="list">
+          <h3>2 November</h3>
+          <hr />
+          <small>What is lorem ipsum lorem ipsum is simply dummy text of the printing and typesetting industry lorem ipsum has been the industry's standard dummy text ever since the 1500s when an unknown printer took and scrambled it to make a type it to make a type specimen book it has?</small>
+        </div>
+        <div className="circle">B</div>
+        <div className="circleB">
+
+        </div>
+        <div className="line">
+
+        </div>
+
+        <div className="list2">
+          <h3>2 November</h3>
+          <hr />
+          <p>What is lorem ipsum lorem ipsum is simply dummy text of the printing and typesetting industry lorem ipsum has been the industry's standard dummy text ever since the 1500s when an unknown printer</p>
+        </div>
+        <h1 className='head3'>December 2019</h1>
+        <div className="circle2">
+          <h3>Creating</h3>
+          <p>What is lorem ipsum lorem ipsum is simply dummy text of the printing and typesetting industry lorem ipsum has been the industry's standard dummy text ever since the 1500s when an unknown printer took and scrambled it to make a type it to make a type specimen book it has?</p>
+        </div>
+
+        <div className="list3">
+          <h3>2 November</h3>
+          <hr />
+          <p>What is lorem ipsum lorem ipsum is simply dummy text of the printing and typesetting industry lorem ipsum has been the industry's standard dummy text ever since the 1500s when an unknown printer</p>
+        </div>
+
+        <div className="circleB2">
+
+        </div>
+        <div className="line2">
+
+        </div>
+        <div className="shape">
+
+        </div>
+        <h1 className='head4'>App completed</h1>
+        <p className='para'>
+          What is lorem ipsum lorem ipsum is simply dummy text of the printing
+        </p>
+        <h1 className='head5'>December2020</h1>
+        <div className="circle3">
+          <h3>Launch</h3>
+          <p>What is lorem ipsum lorem ipsum is simply dummy text of the printing and typesetting industry lorem ipsum has been the industry's standard dummy text ever since the 1500s when an unknown printer took and scrambled it to make a type it to make a type specimen book it has?</p>
+        </div>
+        <h1 className='head6'>App Launch</h1>
+        <div className="list4">
+          <h3 >2 November</h3>
+          <hr />
+          <p>What is lorem ipsum lorem ipsum is simply dummy text of the printing and typesetting industry lorem ipsum has been the industry's standard dummy text ever since the 1500s when an unknown printer</p>
+        </div>
+        <div className="circleB3">
+
+        </div>
+        <div className="line3">
+        </div>
+        <h1 className="head7">Continues...</h1>
+      </div>
+    </div>
+  );
+}
+
+export default App;
